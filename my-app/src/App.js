b5727f34337b07552c2dc79161da7e2f31c48557
Aliciaasmbc <alicia.apreala@gmail.com>
2023-12-17 23:34:35 +0000

    created footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 
 import Weather from "./Weather";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Weather />
+        <Footer />
       </header>
     </div>
   );
---
+++ my-app/src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+export default function footer() {
+  return (
+    <p>
+      This project is <a href="/">Open-sourced on Github</a>
+    </p>
+  );
+}
